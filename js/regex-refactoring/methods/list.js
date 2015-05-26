061b6784641112b29a38311818d0b17101439696
Kamil Mielnik <kamil.mielnik@pega.com>
2015-05-26 16:48:35 +0200

    Code Smell: TODO Comments

--- js/regex-refactoring/methods/list.js
+++ js/regex-refactoring/methods/list.js
@@ -1,13 +1,15 @@
 define([
     'regex-refactoring/methods/recompose-conditional',
     'regex-refactoring/methods/remove-redundant-conditional',
-    'regex-refactoring/methods/long-parameter-list'
-], function (DefaultInitializer, RemoveRedundantConditional, LongParameterList) {
+    'regex-refactoring/methods/long-parameter-list',
+    'regex-refactoring/methods/todo-comment'
+], function (DefaultInitializer, RemoveRedundantConditional, LongParameterList, TODOComment) {
     'use strict';
 
     return [
         DefaultInitializer,
         RemoveRedundantConditional,
-        LongParameterList
+        LongParameterList,
+        TODOComment
     ];
 });
